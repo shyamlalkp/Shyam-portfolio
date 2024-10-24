c9e4ad377a3b7b6653501b0d567868c54f2121a4
shyamlalkp <146191497+shyamlalkp@users.noreply.github.com>
2024-10-24 21:47:43 +0000

    set shyam portfolio

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -67,7 +67,7 @@ const About = () => {
           textAlign: "center",
         }}
       >
-        <h2>About Myself</h2>
+        <h2>Professional Summary</h2>
         <p className="large">{description}</p>
         <hr />
         <ul
